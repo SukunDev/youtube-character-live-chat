f4a8f77ce276f3087a59c6ca264345e7d5bf4dbc
SukunDev <sukundev32@gmail.com>
2025-02-03 22:04:20 +0700

    Add User-Agent headers to API requests for YouTube channel and live chat

--- app/api/channelId/route.js
+++ app/api/channelId/route.js
@@ -6,8 +6,15 @@ export async function GET(request) {
   const channelId = searchParams.get("channelId");
   try {
     const response = await axios.get(
-      `https://www.youtube.com/channel/${channelId}/live`
+      `https://www.youtube.com/channel/${channelId}/live`,
+      {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        },
+      }
     );
+
     const result = getOptionsFromLivePage(response.data);
 
     return Response.json({
--- app/api/liveChat/route.js
+++ app/api/liveChat/route.js
@@ -19,26 +19,8 @@ export async function GET(request) {
           },
         },
         continuation: decodeURIComponent(continuation),
-      },
-      {
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
-          "X-Browser-Channel": "stable",
-          "X-Browser-Copyright":
-            "Copyright 2025 Google LLC. All rights reserved.",
-          "X-Browser-Validation": "Ty5481SnuDG2fxASUTOpmh9tIrU=",
-          "X-Browser-Year": "2025",
-          Accept:
-            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-          "Content-Type": "application/json",
-          Origin: "https://www.youtube.com",
-          Referer: "https://www.youtube.com/",
-          "Accept-Language": "en-US,en;q=0.9",
-        },
       }
     );
-
     const result = parseChatData(response.data);
     return Response.json({
       status: true,
