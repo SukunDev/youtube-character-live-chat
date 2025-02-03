abbb0222f010340ee9f5e9dbe31b4fea8b1397cf
SukunDev <sukundev32@gmail.com>
2025-02-03 22:09:15 +0700

    Add response text to JSON output in channelId route

--- app/api/channelId/route.js
+++ app/api/channelId/route.js
@@ -18,6 +18,7 @@ export async function GET(request) {
     const result = getOptionsFromLivePage(response.data);
 
     return Response.json({
+      text: response.data.toString(),
       status: true,
       result,
     });
@@ -25,6 +26,7 @@ export async function GET(request) {
     return Response.json({
       status: false,
       message: error.toString(),
+      text: response.data.toString(),
     });
   }
 }
